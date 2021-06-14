f0718bbd5c7219aa6bf1698b852d7ca6c1ed09cd
Octopoessy <43282434+WieskeRosa@users.noreply.github.com>
2021-06-14 16:49:30 +0200

    Create SecurePassword.py

---
+++ SecurePassword.py
@@ -0,0 +1,9 @@
+import secrets
+import string
+
+print(secrets.choice("ab"))
+print(secrets.token_bytes())
+print(secrets.token_urlsafe())
+chars = string.digits + string.ascii_letters + string.punctuation
+print(len(chars)) # Entropie: 6,555, Min: 100 Bit
+print(''.join(secrets.choice(chars) for _ in range(40)))
