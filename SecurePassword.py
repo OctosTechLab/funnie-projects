0c6b1daf381ea3cc1c89c920a345ca50fc60993b
Octo's Tech Lab <43282434+OctosTechLab@users.noreply.github.com>
2022-06-20 21:32:56 +0200

    SecurePassword (45)

--- SecurePassword.py
+++ SecurePassword.py
@@ -6,4 +6,4 @@ print(secrets.token_bytes())
 print(secrets.token_urlsafe())
 chars = string.digits + string.ascii_letters + string.punctuation
 print(len(chars)) # Entropie: 6,555, Min: 100 Bit
-print(''.join(secrets.choice(chars) for _ in range(40)))
+print(''.join(secrets.choice(chars) for _ in range(45)))
